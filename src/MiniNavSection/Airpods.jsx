19e531cd41d029bfc547f4e2b434e9117cb11458
Roop Teja <roopteja112@gmail.com>
2025-01-30 07:50:21 +0530

    updated the filter section

--- src/MiniNavSection/Airpods.jsx
+++ src/MiniNavSection/Airpods.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AirpodsData from '../ShopData/Gadgets/AirPods'
 
-const Airpods = () => {    
+const Airpods = () => { 
+    
+    const [ selectedNames, setSelectedNames ] = useState([]);
+    const [ selectedRating, setSelectedRating ] = useState([]);
+
+    const handleNameChange = (value) => {
+        setSelectedNames((selectedNames) =>
+            selectedNames.includes(value)
+                ? selectedNames.filter((item) => item !== value)
+                : [...selectedNames, value]
+        );
+    };
+
+    const handleRangeChange = (value) => {
+
+        setSelectedRating((selectedRating) =>
+            selectedRating.includes(value) ? 
+            selectedRating.filter((item) => item !== value) :
+            [...selectedRating, value] 
+        );
+
+    };
+
+    const filteredData = AirpodsData.filter((data) => {
+
+        const matchName = 
+            selectedNames.length === 0 ||
+            selectedNames.some((item) => data.name.toLowerCase().includes(item.toLowerCase()));
+
+        const matchRange = 
+            selectedRating.length === 0 || 
+            selectedRating.some((range) => {
+                const [ min, max ] = range.split(" to ").map(Number);
+                return data.price >= min && data.price <= max;
+            })
+        
+        return matchName && matchRange;
+
+    })
+
     return (
-        <div>Airpods</div>
+        <>
+            <div className='min-h-screen'>
+                <div>
+
+                    <label>
+                        <input type='checkbox' value="Apple" onChange={(e) => handleNameChange(e.target.value)} /> 
+                        apple
+                    </label>
+                    <label>
+                        <input type='checkbox' value="boat" onChange={(e) => handleNameChange(e.target.value)} /> 
+                        boat
+                    </label>
+
+                    <label>
+                        <input type='checkbox' value="2000 to 5000" onChange={(e) => handleRangeChange(e.target.value)} /> 
+                        2,000 to 5,000
+                    </label>
+                </div>
+                <div>
+                    {
+                        filteredData.length > 0 ? 
+                        (
+                            filteredData.map((item) => (
+                                <div>
+                                    <img className='h-16' src={item.imageURL} alt={item.name} />
+                                    <p>₹ {item.price.toLocaleString("hi-IN")} /-</p>
+                                </div>    
+                            ))
+                        ):
+                        (<p>No products available</p>)
+                    }
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/MiniNavSection/Laptops.jsx
+++ src/MiniNavSection/Laptops.jsx
@@ -1,10 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react';
+import LaptopsData from '../ShopData/Electronics/Laptop';
 
 const Laptops = () => {
-        
-    return (
-        <div>Laptops</div>
-    )
-}
+    const [selectedNames, setSelectedNames] = useState([]);
+    const [selectedRanges, setSelectedRanges] = useState([]);
+    const [selectedRating, setSelectedRating] = useState([]);
 
-export default Laptops
+    const handleNameChange = (value) => {
+        setSelectedNames((selectedNames) =>
+            selectedNames.includes(value)
+                ? selectedNames.filter((item) => item !== value)
+                : [...selectedNames, value]
+        );
+    };
+
+    const handleRangeChange = (value) => {
+        setSelectedRanges((selectedRanges) =>
+            selectedRanges.includes(value)
+                ? selectedRanges.filter((range) => range !== value)
+                : [...selectedRanges, value]
+        );
+    };
+
+    const handleStarRating = (value) => {
+        setSelectedRating((selectedRating) => 
+            selectedRating.includes(value) ?
+            selectedRating.filter((item) => item !== value):
+            [...selectedRating, value]
+        );
+    };
+
+
+    const filteredData = LaptopsData.filter((data) => {
+        
+        const matchName = 
+            selectedNames.length === 0 ||
+            selectedNames.some((item) => data.name.toLowerCase().includes(item.toLowerCase()));
+
+        const matchRange =
+            selectedRanges.length === 0 ||
+            selectedRanges.some((range) => {
+                const [min, max] = range.split(" to ").map(Number);
+                return data.price >= min && data.price <= max;
+            });
+        
+        const matchRating = 
+            selectedRating.length === 0 || 
+            selectedRating.some((rating) => {
+                const [ min, max ] = rating.split(" to ").map(Number);
+                return data.rating >= min && data.rating <= max;
+            })
+
+        return matchName && matchRange && matchRating;
+    });
+
+    return (
+        <div className="min-h-screen">
+            <div>
+                <div>
+                    <label>
+                        <input type="checkbox" value="HP" onChange={(e) => handleNameChange(e.target.value)} />
+                        HP
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type='checkbox' value="40000 to 55000" onChange={(e) => handleRangeChange(e.target.value)} />
+                        40000 to 55000
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type='checkbox' value="4.0 to 5" onChange={(e) => handleStarRating(e.target.value)} />
+                        4.0 to 5
+                    </label>
+                    <label>
+                        <input type='checkbox' value="3.0 to 4.0" onChange={(e) => handleStarRating(e.target.value)} />
+                        3.0 to 4.0
+                    </label>
+                    <label>
+                    <input type='checkbox' value="1.0 to 3.0" onChange={(e) => handleStarRating(e.target.value)} />
+                        1.0 to 3.0
+                    </label>
+                </div>
+            </div>
+            <div>
+                {filteredData.length > 0 ? (
+                    filteredData.map((item) => (
+                        <div key={item.id}>
+                            <img className="h-20" src={item.imageURL} alt={item.name} />
+                            <p>{item.name}</p>
+                            <span>{item.price.toLocaleString("hi-IN")}</span>
+                        </div>
+                    ))
+                ) : (
+                    <p>No products are available</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Laptops;
